46bc1c09e739cd62fb8ca0a4afb55a4f3404148e
Aiazhkkaa <ajnat.777@gmail.com>
2026-02-24 17:55:09 +0500

    Cleanup: remove mistakenly committed Angular project from repo root

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home';
-import { ProductListComponent } from './components/product-list/product-list';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'products', component: ProductListComponent },
-  { path: '**', redirectTo: '' }
-];
